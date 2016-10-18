c428500221ef661688d706c02443f0b39c8cb28b
Williams, Gareth (IM&T, Clayton) <Gareth.Williams@csiro.au>
2016-10-18 22:06:30 +1100

    introduce entrypoint for olfs

---
+++ olfs/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#set -exv
+set -e
+echo "$@"
+
+if [ "${1:0:1}" = '-' ]; then
+	set -- catalina.sh run
+fi
+
+if [ "$1" = 'catalina.sh' ]; then
+
+	# modify viewers.xml.conf based on environment variable before startup
+	if [ "$OLFS_WMS_VIEWERS_HOSTPORT" ]; then
+		sed -i "s/OLFS_WMS_VIEWERS_HOSTPORT/$OLFS_WMS_VIEWERS_HOSTPORT/" ${CATALINA_HOME}/webapps/opendap/WEB-INF/conf/viewers.xml
+	fi
+
+	#exec gosu tomcat "$@"
+fi
+
+exec "$@"
--- prime.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#HP=sc-01-cdc.it.csiro.au:8080
-HP=${1:-localhost:8080}
-sed "s/\$HOSTPORT/$HP/g" olfs/fix_viewers.xml.pl.template > olfs/fix_viewers.xml.pl
