40ad65ca7c834d18ecb547a328beeb8cc55a9fd1
ndp-opendap <ndp@opendap.org>
2017-06-14 16:11:29 -0700

    Added simple build, startuo and shutdown scripts.

---
+++ buildit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t bes-3.18.0-static besd
+docker build -t olfs-1.16.3 olfs
--- startup.sh
+++ startup.sh
@@ -1,9 +1,10 @@
-#!bin/bash
+#!/bin/bash
 
 
-# Run the besd container (works)
-docker run -itd -p 10022:10022 --name=besd bes-3.18.0-static
+# Run the besd container 
+docker run -itd -p 10022:10022 -v $prefix/share/hyrax:/usr/share/hyrax --name=besd bes-3.18.0-static
 
-# Run the OLFS and "link" it to the besd container
-docker run -itd -p 8080:8080 -v /tmp:/usr/local/tomcat/webapps/opendap/WEB-INF/conf/logs --link besd --name=olfs olfs-1.16.3 
+# Run the OLFS container and "link" it to the besd container
+mkdir -p $prefix/var/olfs_logs
+docker run -itd -p 8080:8080 -v $prefix/var/olfs_logs:/usr/local/tomcat/webapps/opendap/WEB-INF/conf/logs --link besd --name=olfs olfs-1.16.3 
 
