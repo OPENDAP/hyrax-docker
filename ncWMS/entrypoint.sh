2575bb7ce5c223c20145f29fbf40d4b4cfbcd238
ndp-opendap <ndp@opendap.org>
2017-07-12 11:14:44 -0700

    Worked on developer-compose.yml and it now works and launches esd, olfs, and ncWMS

--- ncWMS/entrypoint.sh
+++ ncWMS/entrypoint.sh
@@ -2,6 +2,7 @@
 #set -exv
 set -e
 echo "entrypoint.sh commmandline: $@"
+echo "user: "`whoami`
 
 echo "CATALINA_HOME: "$CATALINA_HOME
 ls -l "$CATALINA_HOME"
@@ -13,10 +14,10 @@ set -x
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
 # startup.sh -security
-startup.sh # Without -security will ncWMS work?
+startup.sh # Without --security makes ncWMS because the CORS filter get's tangled up with the security manager.
 
-tail -f /usr/local/tomcat/logs/catalina.out
+# tail -f /usr/local/tomcat/logs/catalina.out
 # never exit
-while true; do sleep 10; echo -n "."; done
+while true; do sleep 60; echo -n "."; done
 
 
