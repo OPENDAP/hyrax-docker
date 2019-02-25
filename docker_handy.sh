fb8b5a75fa3daba15810e2fdf07d47f52656cc19
Nathan Potter <ndp@opendap.org>
2019-02-25 15:58:55 -0800

    Adding a bash script containing functions. Source it to have a small collection of terse commands to manipulate docker.

---
+++ docker_handy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Docker Tricks
+# Source this file and then use the functions on the commandline
+#
+
+
+#
+# Stop all running containers
+#
+function dhalt() {
+    docker stop $(docker ps -aq);
+}
+
+#
+# Remove all containers
+#
+function drmc(){
+    docker rm $(docker ps -aq);
+}
+
+#
+# Remove all images
+#
+function drmi(){
+    docker rmi $(docker images -q);
+}
+
+#
+# List all containers (only IDs)
+#
+function dlist(){
+    docker ps -aq;
+}
+
+#
+# Interactive Login Shell
+#
+function dlogin(){
+   docker exec -ti $1 bash;
+} 
+
+#
+# stop, cleanup container and image, build, run, login interactive shell
+#
+function dscbrl() {
+    package=$1;
+    tag=`echo "${package}" | tr '[:upper:]' '[:lower:]'`
+    docker container stop ${tag}
+    docker rm $(docker ps -aq)
+    docker rmi ${tag}
+    docker build -t ${tag} ${package}
+    docker run --name ${tag} -d -p 8080:8080 ${tag}
+    docker exec -ti ${tag} bash
+}
