00db1976d5755c72f0f3a416fe9b72fcd78ec8a7
ndp-opendap <ndp@opendap.org>
2020-03-03 05:41:31 -0800

    Added bc to snapshot and template images (small, useful) Groomed creds injection script in entrypoint.sh Add creds injection script to template entrypoint.sh

--- hyrax-snapshot/hyrax/entrypoint.sh
+++ hyrax-snapshot/hyrax/entrypoint.sh
@@ -12,21 +12,34 @@ echo "Greetings, I am "`whoami`".";   >&2
 set -e
 #set -x
 
-# Only create the .netrc file if all three environment variables are defined
-if [ $HOST ] &&  [ $USERNAME ] && [ $PASSWORD ]; then
-    echo "machine $HOST" >> ~/.netrc
-    echo "login $USERNAME" >> ~/.netrc
-    echo "password $PASSWORD" >> ~/.netrc
+################################################################################
+# Inject one set of credentials into .netrc
+# Only touch the .netrc file if all three environment variables are defined
+#
+if [ $HOST ] && [ -n $HOST ]  &&  \
+   [ $USERNAME ] &&  [ -n $USERNAME ] && \
+   [ $PASSWORD ] && [ -n $PASSWORD ]; then
+    echo "machine ${HOST}" >> ~/.netrc
+    echo "login ${USERNAME}" >> ~/.netrc
+    echo "password ${PASSWORD}" >> ~/.netrc
 fi
+################################################################################
 
-# Hyrax-in-the-cloud configuration
-# Only create the /etc/bes/site.conf file if all the necessary environment variables are defined
-if [ $S3_DISTRIBUTION_ENDPOINT ] &&  [ $S3_REFRESH_MARGIN ] && [ $S3_AWS_REGION ] && [ $S3_BASE_URL ]; then
-    echo "NGAP.S3.distribution.endpoint.url=$S3_DISTRIBUTION_ENDPOINT" >> /etc/bes/site.conf
-    echo "NGAP.S3.refresh.margin=$S3_REFRESH_MARGIN" >> /etc/bes/site.conf
-    echo "NGAP.S3.region=$S3_AWS_REGION" >> /etc/bes/site.conf
-    echo "NGAP.S3.url.base=$S3_BASE_URL" >> /etc/bes/site.conf
+################################################################################
+# Inject an NGAP Cumulus Configuration
+# Only create the /etc/bes/site.conf file if all the necessary environment
+# variables are defined
+#
+if [ $S3_DISTRIBUTION_ENDPOINT ] && [ -n $S3_DISTRIBUTION_ENDPOINT ] &&  \
+   [ $S3_REFRESH_MARGIN ] && [ -n $S3_REFRESH_MARGIN ] && \
+   [ $S3_AWS_REGION ] && [ -n $S3_AWS_REGION ] && \
+   [ $S3_BASE_URL ] && [ -n $S3_BASE_URL ]; then
+    echo "NGAP.S3.distribution.endpoint.url=${S3_DISTRIBUTION_ENDPOINT}" >> /etc/bes/site.conf
+    echo "NGAP.S3.refresh.margin=${S3_REFRESH_MARGIN}" >> /etc/bes/site.conf
+    echo "NGAP.S3.region=${S3_AWS_REGION}" >> /etc/bes/site.conf
+    echo "NGAP.S3.url.base=${S3_BASE_URL}" >> /etc/bes/site.conf
 fi
+################################################################################
 
 if [ $SERVER_HELP_EMAIL ] && [ -n $SERVER_HELP_EMAIL ] ; then    
     echo "Found exisiting SERVER_HELP_EMAIL: $SERVER_HELP_EMAIL"  
