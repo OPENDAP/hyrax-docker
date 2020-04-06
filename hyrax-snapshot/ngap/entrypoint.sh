1766ffcb415f2a692882daee0d896c298a37d821
ndp-opendap <ndp@opendap.org>
2020-04-06 05:38:09 -0700

    Added input santization for netrc HOST

--- hyrax-snapshot/ngap/entrypoint.sh
+++ hyrax-snapshot/ngap/entrypoint.sh
@@ -17,7 +17,7 @@ set -e
 # Only touch the .netrc file if all three environment variables are defined
 #
 if [ -n "${HOST}" ]  &&  [ -n "${USERNAME}" ] &&  [ -n "${PASSWORD}" ]; then
-    echo "machine ${HOST}"          >> /etc/bes/ngap_netrc
+    echo "machine ${HOST}" | sed -e "s_https://__g"  -e "s_http://__g" >> /etc/bes/ngap_netrc
     echo "    login ${USERNAME}"    >> /etc/bes/ngap_netrc
     echo "    password ${PASSWORD}" >> /etc/bes/ngap_netrc
     cp /etc/bes/ngap_netrc ~/.netrc
