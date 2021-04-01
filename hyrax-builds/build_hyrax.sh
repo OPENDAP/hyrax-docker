875ca1c8eb7811952d51d85625fcb5b69cd2b38c
James Gallagher <jgallagher@opendap.org>
2021-04-01 17:38:20 -0600

    Removed the build_hyrax.sh script because it was evil

--- hyrax-builds/build_hyrax.sh
+++
@@ -1,9 +0,0 @@
-docker build --build-arg RELEASE_DATE=1 \
-       --build-arg AWS_ACCESS_KEY_ID= \
-       --build-arg AWS_SECRET_ACCESS_KEY= \
-       --build-arg LIBDAP_VERSION=3.20.7-21 \
-       --build-arg BES_VERSION=3.20.8-48 \
-       --build-arg OLFS_VERSION=1.18.8-79 \
-       --build-arg HYRAX_VERSION=1.16.3-49 \
-       --tag hyrax-foo hyrax --verbose
-
