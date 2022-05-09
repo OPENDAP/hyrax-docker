86bf65ecb9bae2e384a92ce41170217fc6c0d745
ndp-opendap <ndp@opendap.org>
2022-05-09 14:28:56 -0400

    Fixed(?) find call by switching to -mmin

--- hyrax-builds/hyrax/cleanup_files.sh
+++ hyrax-builds/hyrax/cleanup_files.sh
@@ -9,7 +9,7 @@
 #
 verbose="${verbose:-}"
 default_dir="/tmp/hyrax_fonc"
-default_interval="1h"
+default_interval="60" # minutes
 default_log="/var/log/bes/nc_cleanup.log"
 
 if test -n "${verbose}"; then echo "##########################################################"; fi
@@ -19,33 +19,20 @@ if test -n "${verbose}"; then echo "# $0"; echo "#"; fi
 TARGET_DIR="${1:-$default_dir}"
 if test -n "${verbose}"; then echo "#   TARGET_DIR: ${TARGET_DIR}"; fi
 
-INTERVAL="${2:-$default_interval}"
-if test -n "${verbose}"; then echo "#     INTERVAL: ${INTERVAL}"; fi
+INTERVAL="${2:-$default_interval}" # minutes
+if test -n "${verbose}"; then echo "#     INTERVAL: ${INTERVAL}m"; fi
 
 CLEANUP_LOG="${3:-$default_log}"
 if test -n "${verbose}"; then echo "#  CLEANUP_LOG: ${CLEANUP_LOG}"; fi
 
-
-SLEEP_TIME=$(echo "${INTERVAL}" | awk '
-    BEGIN{ m["s"]=1;m["m"]=60;m["h"]=3600;m["d"]=86400;}
-    {
-        time=$1;
-        units=substr(time,length(time),1);
-        #print "m["units"]: "m[units];
-        #print "units: "units
-        sub(units,"",time);
-        #print "time: "time
-        sleeptime=m[units]*time;
-        print sleeptime;
-    }')
+SLEEP_TIME=$(echo "${INTERVAL}*60" | bc)
 if test -n "${verbose}"; then echo "#   SLEEP_TIME: ${SLEEP_TIME}s"; fi
 if test -n "${verbose}"; then echo "#"; fi
 
 
 while true 
 do
-
-    orphaned_files=$(find ${TARGET_DIR} -type f -mtime +${INTERVAL})
+    orphaned_files=$(find ${TARGET_DIR} -type f -mmin ${INTERVAL})
     if test -n "${verbose}"; then echo "orphaned_files: "${orphaned_files}; fi
 
     if test -n "${orphaned_files}"; 
