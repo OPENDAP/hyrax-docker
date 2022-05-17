906aa093e7dd9ef426669295c34288c403809e68
ndp-opendap <ndp@opendap.org>
2022-05-17 16:33:15 -0400

    Restoring CentOS-7 docker build.

--- hyrax-builds/hyrax/entrypoint.sh
+++ hyrax-builds/hyrax/entrypoint.sh
@@ -135,8 +135,7 @@ echo "The BES is UP! pid: $besd_pid"; >&2
 
 
 # Start Tomcat process
-#/usr/libexec/tomcat/server start > /var/log/tomcat/console.log 2>&1 &
-/usr/local/tomcat/bin/startup.sh > /var/log/tomcat/console.log 2>&1 &
+/usr/libexec/tomcat/server start > /var/log/tomcat/console.log 2>&1 &
 status=$?
 tomcat_pid=$!
 if [ $status -ne 0 ]; then
