918363047ad999afb9c96d09e656b35e5355352d
ndp-opendap <ndp@opendap.org>
2022-06-28 06:41:39 -0700

    Overhauled entrypoint.sh for ngap

--- hyrax-builds/ngap/entrypoint.sh
+++ hyrax-builds/ngap/entrypoint.sh
@@ -7,24 +7,88 @@
 # set -e #  Exit on error.
 
 #echo "entrypoint.sh  command line: \"$@\""
-echo "############################## HYRAX ##################################" >&2
-echo "Greetings, I am "`whoami`"." >&2
+echo "############################## HYRAX ################################" >&2
+echo "# Greetings, I am "`whoami`"." >&2
 set -e
 #set -x
+echo "#"
+
+echo "#      Checking AWS CLI: "
+aws configure list 2>&1 | awk '{print "#    "$0;}' >&2
+status=$?
+if test $status -ne 0 ; then
+    echo "WARNING: Problem with AWS CLI! (status: ${status})" >&2
+fi
+
+echo  "#          JAVA VERSION: "
+java -version 2>&1 | awk '{print "#                       "$0;}' >&2
+
+echo "#--------------------------------------------------------------------" >&2
+export JAVA_HOME=${JAVA_HOME:-"/etc/alternatives/jre"}
+echo "#             JAVA_HOME: ${JAVA_HOME}" >&2
+
+export SLEEP_INTERVAL=${SLEEP_INTERVAL:-60}
+echo "#        SLEEP_INTERVAL: ${SLEEP_INTERVAL} seconds." >&2
+
+export SERVER_HELP_EMAIL=${SERVER_HELP_EMAIL:-"not_set"}
+echo "#     SERVER_HELP_EMAIL: ${SERVER_HELP_EMAIL}" >&2
+
+export FOLLOW_SYMLINKS=${FOLLOW_SYMLINKS:-"not_set"}
+echo "#       FOLLOW_SYMLINKS: ${FOLLOW_SYMLINKS}" >&2
+
+export NCWMS_BASE=${NCWMS_BASE:-"https://localhost:8080"}
+echo "#            NCWMS_BASE: ${NCWMS_BASE}" >&2
+
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-"<not set>"}
+echo "# AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}" >&2
+
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-"<not set>"}
+echo "#     AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}" >&2
+
+export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-"<not set>"}
+echo "#    AWS_DEFAULT_REGION: ${AWS_DEFAULT_REGION}" >&2
+
+export CATALINA_HOME=${CATALINA_HOME:-"NOT_SET"}
+echo "#         CATALINA_HOME: ${CATALINA_HOME}" >&2
+
+export DEPLOYMENT_CONTEXT=${DEPLOYMENT_CONTEXT:-"ROOT"}
+echo "#    DEPLOYMENT_CONTEXT: ${DEPLOYMENT_CONTEXT}" >&2
+
+
+export OLFS_CONF_DIR="${CATALINA_HOME}/webapps/${DEPLOYMENT_CONTEXT}/WEB-INF/conf"
+echo "#         OLFS_CONF_DIR: ${OLFS_CONF_DIR}" >&2
+
+
+export TOMCAT_CONTEXT_FILE="/usr/share/tomcat/conf/context.xml"
+echo "#   TOMCAT_CONTEXT_FILE: ${TOMCAT_CONTEXT_FILE}" >&2
+
+export NETRC_FILE="/etc/bes/ngap_netrc"
+echo "#            NETRC_FILE: ${NETRC_FILE}" >&2
+
+export BES_SITE_CONF_FILE="/etc/bes/site.conf"
+echo "#    BES_SITE_CONF_FILE: ${BES_SITE_CONF_FILE}" >&2
+
+export BES_LOG_FILE="/var/log/bes/bes.log"
+echo "#          BES_LOG_FILE: ${BES_LOG_FILE}" >&2
+
+
 
 ################################################################################
 # Inject one set of credentials into .netrc
-# Only touch the .netrc file if all three environment variables are defined
+# Only modify the .netrc file if all three environment variables are defined
 #
-if [ -n "${HOST}" ]  &&  [ -n "${USERNAME}" ] &&  [ -n "${PASSWORD}" ]; then
+if test -n "${HOST}"  &&  test -n "${USERNAME}"  &&  test -n "${PASSWORD}" ; then
+    echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- " >&2
+    echo "# Updating netrc file: ${NETRC_FILE}" >&2
     # machine is a domain name or a ip address, not a URL.
-    echo "machine ${HOST}" | sed -e "s_https:__g"  -e "s_http:__g" -e "s+/++g" >> /etc/bes/ngap_netrc
-    echo "    login ${USERNAME}"    >> /etc/bes/ngap_netrc
-    echo "    password ${PASSWORD}" >> /etc/bes/ngap_netrc
-    chown bes:bes /etc/bes/ngap_netrc
-    chmod 400 /etc/bes/ngap_netrc
-    ls -l /etc/bes/ngap_netrc  >&2
-    cat /etc/bes/ngap_netrc >&2
+    echo "machine ${HOST}" | sed -e "s_https:__g"  -e "s_http:__g" -e "s+/++g" >> "${NETRC_FILE}"
+    echo "    login ${USERNAME}"    >> "${NETRC_FILE}"
+    echo "    password ${PASSWORD}" >> "${NETRC_FILE}"
+    chown bes:bes "${NETRC_FILE}"
+    chmod 400 "${NETRC_FILE}"
+    ls -l "${NETRC_FILE}"  >&2
+    cat "${NETRC_FILE}" >&2
+    echo "#" >&2
 fi
 ################################################################################
 
@@ -33,13 +97,15 @@ fi
 # Inject user-access.xml document to define the servers relationship to
 # EDL and the user access rules.
 #
-echo "CATALINA_HOME: ${CATALINA_HOME}" >&2
-user_access_xml_file="/usr/share/tomcat/webapps/${DEPLOYMENT_CONTEXT}/WEB-INF/conf/user-access.xml"
 # Test if the user-access.xml env variable is set (by way of not unset) and not empty
-if [ -n "${USER_ACCESS_XML}" ] ; then
-    echo "${USER_ACCESS_XML}" > ${user_access_xml_file}
-    echo "${user_access_xml_file} -" >&2
-    cat ${user_access_xml_file} >&2
+if test -n "${USER_ACCESS_XML}"  ; then
+    echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- " >&2
+    USER_ACCESS_XML_FILE="${OLFS_CONF_DIR}/user-access.xml"
+    echo "# Updating OLFS user access controls: ${USER_ACCESS_XML_FILE}" >&2
+    echo "${USER_ACCESS_XML}" > ${USER_ACCESS_XML_FILE}
+    echo "${USER_ACCESS_XML_FILE} -" >&2
+    cat ${USER_ACCESS_XML_FILE} >&2
+    echo "#" >&2
 fi
 ################################################################################
 
@@ -48,13 +114,16 @@ fi
 # Inject BES configuration site.conf document to configure the BES to operate
 # in the NGAP environment.
 #
-bes_site_conf_file="/etc/bes/site.conf"
 # Test if the bes.conf env variable is set (by way of not unset) and not empty
-if [ -n "${BES_SITE_CONF}" ] ; then
-    # echo "${BES_SITE_CONF}" > ${bes_site_conf_file}
-    echo "${BES_SITE_CONF}" | sed -e "s+BES.LogName=stdout+BES.LogName=/var/log/bes/bes.log+g" > ${bes_site_conf_file}
-    echo "${bes_site_conf_file} - " >&2
-    cat ${bes_site_conf_file} >&2
+if test -n "${BES_SITE_CONF}" ; then
+    echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- " >&2
+    echo "# Updating BES site.conf: ${BES_SITE_CONF_FILE}" >&2
+    # echo "${BES_SITE_CONF}" > ${BES_SITE_CONF_FILE}
+    # @TODO THis seems like a crappy hack, we should just change the source file in BitBucket to be correct
+    echo "${BES_SITE_CONF}" | sed -e "s+BES.LogName=stdout+BES.LogName=${BES_LOG_FILE}+g" > ${BES_SITE_CONF_FILE}
+    echo "${BES_SITE_CONF_FILE} - " >&2
+    cat ${BES_SITE_CONF_FILE} >&2
+    echo "#" >&2
 fi
 ################################################################################
 
@@ -63,12 +132,14 @@ fi
 # Inject Tomcat's context.xml configuration document to configure the Tomcat to
 # utilize Session Management in the NGAP environment.
 #
-tomcat_context_xml_file="/usr/share/tomcat/conf/context.xml"
 # Test if the bes.conf env variable is set (by way of not unset) and not empty
-if [ -n "${TOMCAT_CONTEXT_XML}" ] ; then
-    echo "${TOMCAT_CONTEXT_XML}" > ${tomcat_context_xml_file}
-    echo "${tomcat_context_xml_file} - " >&2
-    cat ${tomcat_context_xml_file} >&2
+if test -n "${TOMCAT_CONTEXT_XML}" ; then
+    echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- " >&2
+    echo "# Tomcat context.xml file: ${TOMCAT_CONTEXT_FILE}" >&2
+    echo "${TOMCAT_CONTEXT_XML}" > ${TOMCAT_CONTEXT_FILE}
+    echo "${TOMCAT_CONTEXT_FILE} - " >&2
+    cat ${TOMCAT_CONTEXT_FILE} >&2
+    echo "#" >&2
 fi
 ################################################################################
 
@@ -83,72 +154,57 @@ fi
 #   [ -n "${S3_AWS_REGION}" ] && \
 #   [ -n "${S3_BASE_URL}" ]; then
 #
-#  echo "Amending BES site.conf file." >&2
+#    echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- " >&2
+#    echo "# Amending BES site.conf file: ${BES_SITE_CONF_FILE}" >&2
 #  {
 #    echo "NGAP.S3.distribution.endpoint.url=${S3_DISTRIBUTION_ENDPOINT}"
 #    echo "NGAP.S3.refresh.margin=${S3_REFRESH_MARGIN}"
 #    echo "NGAP.S3.region=${S3_AWS_REGION}"
 #    echo "NGAP.S3.url.base=${S3_BASE_URL}"
-#  } | tee -a "${bes_site_conf_file}" >&2
+#  } | tee -a "${BES_SITE_CONF_FILE}" >&2
+#    echo "#" >&2
 #fi
 ################################################################################
 
-export JAVA_HOME="${JAVA_HOME:-/etc/alternatives/jre}"
-echo "JAVA_HOME: ${JAVA_HOME}" >&2
 
-export SLEEP_INTERVAL="${SLEEP_INTERVAL:-60}"
-echo "SLEEP_INTERVAL: ${SLEEP_INTERVAL} seconds." >&2
 
-export SERVER_HELP_EMAIL="${SERVER_HELP_EMAIL:-not_set}"
-echo "SERVER_HELP_EMAIL: ${SERVER_HELP_EMAIL}" >&2
-
-export FOLLOW_SYMLINKS="${FOLLOW_SYMLINKS:-not_set}"
-echo "FOLLOW_SYMLINKS: ${FOLLOW_SYMLINKS}" >&2
-
-export NCWMS_BASE="${NCWMS_BASE:-https://localhost:8080}"
-echo "NCWMS_BASE: ${NCWMS_BASE}" >&2
-
-export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-<not set>}"
-echo "AWS_SECRET_ACCESS_KEY is ${AWS_SECRET_ACCESS_KEY}" >&2
-
-export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-<not set>}"
-echo "AWS_ACCESS_KEY_ID is ${AWS_ACCESS_KEY_ID}" >&2
-
-export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-<not set>}"
-echo "AWS_DEFAULT_REGION is ${AWS_DEFAULT_REGION}" >&2
-
+################################################################################
+#
+# Process commandline arguments
+#
+#
 debug=false;
 
 while getopts "de:sn:" opt; do
-  echo "Processing command line opt: ${opt}" >&2
+  echo "# Processing command line opt: ${opt}" >&2
   case $opt in
     e)
       export SERVER_HELP_EMAIL=$OPTARG
-      echo "Set server admin contact email to: ${SERVER_HELP_EMAIL}" >&2
+      echo "# Set SERVER_HELP_EMAIL: ${SERVER_HELP_EMAIL}" >&2
       ;;
     s)
       export FOLLOW_SYMLINKS="Yes"
-      echo "Set FollowSymLinks to: ${FOLLOW_SYMLINKS}" >&2
+      echo "# Set FOLLOW_SYMLINKS: ${FOLLOW_SYMLINKS}" >&2
       ;;
     n)
       export NCWMS_BASE=$OPTARG
-      echo "Set ncWMS public facing service base to : ${NCWMS_BASE}" >&2
+      echo "# Set NCWMS_BASE: ${NCWMS_BASE}" >&2
       ;;
     d)
       export debug=true
-      echo "Debug is enabled" >&2
+      echo "# Debug is enabled" >&2
       ;;
     k)
       export AWS_SECRET_ACCESS_KEY="${OPTARG}"
-      echo "Found command line value for AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}" >&2;
+      echo "Set AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}" >&2;
       ;;
     i)
       export AWS_ACCESS_KEY_ID="${OPTARG}"
-      echo "Found command line value for AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}" >&2;
+      echo "# Set AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}" >&2;
       ;;
     r)
       export AWS_DEFAULT_REGION="${OPTARG}"
-      echo "Found command line value for AWS_DEFAULT_REGION: ${AWS_DEFAULT_REGION}" >&2;
+      echo "# Set AWS_DEFAULT_REGION: ${AWS_DEFAULT_REGION}" >&2;
       ;;
 
     \?)
@@ -167,15 +223,25 @@ while getopts "de:sn:" opt; do
       ;;
   esac
 done
+#
+# END Command Line Processing
+################################################################################
 
 if test "${debug}" = "true" ; then
-    echo "CATALINA_HOME: ${CATALINA_HOME}"  >&2
-    ls -l "$CATALINA_HOME" "$CATALINA_HOME/bin"  >&2
+    echo "# CATALINA_HOME: ${CATALINA_HOME}"  >&2
+    ls -l "${CATALINA_HOME}" >&2
+    echo "# CATALINA_HOME/bin: ${CATALINA_HOME}/bin"  >&2
+    ls -l "${CATALINA_HOME}/bin"  >&2
 fi
 
-DEFAULT_CONF_DIR="${CATALINA_HOME}/webapps/${DEPLOYMENT_CONTEXT}/WEB-INF/conf"
-VIEWERS_XML="${DEFAULT_CONF_DIR}/viewers.xml"
+
+################################################################################
+#
+#  Configuring NcWMS
+#
+VIEWERS_XML="${OLFS_CONF_DIR}/viewers.xml"
 if test "${debug}" = "true" ; then
+    echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- " >&2
     echo "NCWMS: Using NCWMS_BASE: ${NCWMS_BASE}"  >&2
     echo "NCWMS: Setting ncWMS access URLs in viewers.xml (if needed)."  >&2
     ls -l "${VIEWERS_XML}" >&2
@@ -189,39 +255,48 @@ if test "${debug}" = "true" ; then
     echo "${VIEWERS_XML} - "  >&2
     cat "${VIEWERS_XML}" >&2
 fi
+################################################################################
 
-LOGBACK_XML="${DEFAULT_CONF_DIR}/logback.xml"
-NGAP_LOGBACK_XML="${DEFAULT_CONF_DIR}/logback-ngap.xml"
+################################################################################
+#
+# Configure OLFS debug logging if debug is enabled.
 if test "${debug}" = "true" ; then
-    cp "${NGAP_LOGBACK_XML}" "${LOGBACK_XML}"
-    echo "Enabled Logback (slf4j) debug logging for NGAP."  >&2
-    cat "${LOGBACK_XML}"  >&2
+    echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- " >&2
+    echo "# Configuring OLFS to debug logging..."
+    logback_xml="${OLFS_CONF_DIR}/logback.xml"
+    ngap_logback_xml="${OLFS_CONF_DIR}/logback-ngap.xml"
+    cp "${ngap_logback_xml}" "${logback_xml}"
+    echo "# Enabled Logback (slf4j) debug logging for NGAP."  >&2
+    cat "${logback_xml}"  >&2
+    echo "#"  >&2
 fi
+################################################################################
 
-#-------------------------------------------------------------------------------
+################################################################################
+#
 # modify bes.conf based on environment variables before startup.
 #
 if test "${SERVER_HELP_EMAIL}" != "not_set" ; then
+    echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- " >&2
     echo "Setting Admin Contact To: $SERVER_HELP_EMAIL"
     sed -i "s/admin.email.address@your.domain.name/$SERVER_HELP_EMAIL/" /etc/bes/bes.conf
+    echo "#"  >&2
 fi
 if test "${FOLLOW_SYMLINKS}" != "not_set" ; then
+    echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- " >&2
     echo "Setting BES FollowSymLinks to YES." >&2
     sed -i "s/^BES.Catalog.catalog.FollowSymLinks=No/BES.Catalog.catalog.FollowSymLinks=Yes/" /etc/bes/bes.conf
+    echo "#" >&2
 fi
+################################################################################
 
-echo "JAVA VERSION: "
-java -version
 
-aws configure list >&2
-status=$?
-if test $status -ne 0 ; then
-    echo "WARNING: Problem with AWS CLI! (status: ${status})" >&2
-fi
 
 #-------------------------------------------------------------------------------
 # Start the BES daemon process
 # /usr/bin/besdaemon -i /usr -c /etc/bes/bes.conf -r /var/run/bes.pid
+echo "############################## besd #################################" >&2
+echo "#    Starting besd..."
 /usr/bin/besctl start -d "/dev/null,timing" >&2
 status=$?
 if test $status -ne 0 ; then
@@ -229,15 +304,18 @@ if test $status -ne 0 ; then
     exit $status
 fi
 besd_pid=`ps aux | grep /usr/bin/besdaemon | grep -v grep | awk '{print $2;}' - `
-echo "The BES is UP! pid: $besd_pid" >&2
+echo "# The BES is UP! pid: $besd_pid" >&2
+echo "#" >&2
 
 #-------------------------------------------------------------------------------
 # Start Tomcat process
 #
-export OLFS_CONF="${CATALINA_HOME}/webapps/opendap/WEB-INF/conf"
-# mv ${OLFS_CONF}/logback.xml ${OLFS_CONF}/logback.xml.OFF
-echo "Starting Tomcat..." >&2
+echo "########################### tomcat/olfs #############################" >&2
+echo "#    Starting tomcat/olfs..." >&2
+
+# mv ${OLFS_CONF_DIR}/logback.xml ${OLFS_CONF_DIR}/logback.xml.OFF
 #systemctl start tomcat
+
 ${CATALINA_HOME}/bin/startup.sh 2>&1 > /var/log/tomcat/console.log &
 status=$?
 tomcat_pid=$!
@@ -248,7 +326,7 @@ fi
 # When we launch tomcat the initial pid gets "retired" because it spawns a
 # secondary processes.
 initial_pid="${tomcat_pid}"
-echo "Tomcat started, initial pid: ${initial_pid}" >&2
+echo "# Tomcat started, initial pid: ${initial_pid}" >&2
 while test $initial_pid -eq $tomcat_pid
 do
     sleep 1
@@ -258,14 +336,15 @@ do
     echo "tomcat_pid: ${tomcat_pid}" >&2
 done
 # New pid and we should be good to go.
-echo "Tomcat is UP! pid: ${tomcat_pid}" >&2
+echo "# Tomcat is UP! pid: ${tomcat_pid}" >&2
+echo "#" >&2
 
 # TEMPORARY
 /cleanup_files.sh >&2 &
 # TEMPORARY
 
-echo "Hyrax Has Arrived..." >&2
-echo "--------------------------------------------------------------------" >&2
+echo "# Hyrax Has Arrived..." >&2
+echo "#--------------------------------------------------------------------" >&2
 #-------------------------------------------------------------------------------
 while /bin/true; do
     sleep ${SLEEP_INTERVAL}
@@ -299,13 +378,13 @@ while /bin/true; do
     fi
 
     if test $debug = true ; then
-        echo "-------------------------------------------------------------------"  >&2
+        echo "#-----------------------------------------------------------"  >&2
         date >&2
-        echo "BESD_STATUS: $BESD_STATUS  besd_pid:$besd_pid" >&2
-        echo "TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid" >&2
+        echo "# BESD_STATUS: $BESD_STATUS  besd_pid:$besd_pid" >&2
+        echo "# TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid" >&2
     fi
 
-    tail -f /var/log/bes/bes.log | awk -f beslog2json.awk
+    tail -f "${BES_LOG_FILE}" | awk -f beslog2json.awk
 
 done
 #-------------------------------------------------------------------------------
