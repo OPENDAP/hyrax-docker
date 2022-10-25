faae29e53bfe63fd6cdf1756dcf0e2448a0460d8
ndp-opendap <ndp@opendap.org>
2022-10-25 16:39:26 -0400

    Updating docker hub user and passwrd env naes in dploy script

--- deploy_to_docker_hub.sh
+++ deploy_to_docker_hub.sh
@@ -7,7 +7,7 @@ set -e
 OPENDAP_AWS_ACCOUNT=747931985039
 
 echo "Deploying ${SNAPSHOT_IMAGE_TAG} to Docker Hub"
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+echo $DOCKER_HUB_PSWD | docker login -u $DOCKER_HUB_UID --password-stdin
 
 docker push ${SNAPSHOT_IMAGE_TAG}
 docker push ${BUILD_VERSION_TAG}
