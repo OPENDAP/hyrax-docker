5165e4fccded8ff7e33fbde40f956c84cfc430d4
ndp-opendap <ndp@opendap.org>
2022-10-26 11:26:46 -0400

    Debugging deployment script.

--- deploy_to_docker_hub.sh
+++ deploy_to_docker_hub.sh
@@ -6,17 +6,20 @@ set -e
 
 OPENDAP_AWS_ACCOUNT=747931985039
 
-echo "Deploying ${SNAPSHOT_IMAGE_TAG} to Docker Hub"
+echo "Logging into Docker Hub"
 echo $DOCKER_HUB_PSWD | docker login -u $DOCKER_HUB_UID --password-stdin
 
+echo "Deploying ${SNAPSHOT_IMAGE_TAG} to Docker Hub"
 docker push ${SNAPSHOT_IMAGE_TAG}
+echo "Deploying ${BUILD_VERSION_TAG} to Docker Hub"
 docker push ${BUILD_VERSION_TAG}
 
 echo "Docker Hub deployment complete."
 
-echo "Deploying ${SNAPSHOT_IMAGE_TAG} to AWS ECR"
+echo "AWS configuration:""
 aws configure list
 
+echo "Deploying ${SNAPSHOT_IMAGE_TAG} to AWS ECR"
 docker tag ${SNAPSHOT_IMAGE_TAG} ${OPENDAP_AWS_ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/${SNAPSHOT_IMAGE_TAG}
 docker push ${OPENDAP_AWS_ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/${SNAPSHOT_IMAGE_TAG}
 
