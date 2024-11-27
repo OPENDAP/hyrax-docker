b011efc321981a62f74f7ca469623e11d43f3852
ndp-opendap <ndp@opendap.org>
2024-11-27 10:42:35 -0800

    Refinning regression test script for json processing

--- builds/ngap/beslog2json.py
+++ builds/ngap/beslog2json.py
@@ -1,4 +1,27 @@
+# -*- mode: python; c-basic-offset:4 -*-
 #
+# This file is part of the Hyrax Docker Project
+#
+# Copyright (c) 2024 OPeNDAP, Inc.
+# Author: Nathan Potter <ndp@opendap.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+#
+# ----------------------------------------------------------------------------------
 # Program beslog2json.py
 # Converts BES log lines with the formats descrbied below in to
 # a json record.
