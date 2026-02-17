5f5d30caba8e3e71e862d892d622af606f70af42
ndp-opendap <ndp@opendap.org>
2026-02-17 09:40:05 -0800

    Working ngap docker

--- el9-builds/hyrax/entrypoint.sh
+++ el9-builds/hyrax/entrypoint.sh
@@ -7,229 +7,242 @@
 # set -e #  Exit on error.
 
 #echo "entrypoint.sh  command line: \"$@\""
-echo "############################## HYRAX ##################################" >&2
-echo "Greetings, I am "`whoami`" (uid: "`echo ${UID}`")."   >&2
+function loggy(){
+    echo  "$@" | awk '{ print "# "$0;}'  >&2
+}
+
+loggy "############################## HYRAX ##################################"
+loggy "Greetings, I am "`whoami`" (uid: "`echo ${UID}`")."
 # set -e
 # set -x
 
 echo "PythonVersion: "$(python3 --version)
 
 export JAVA_HOME=${JAVA_HOME:-"/etc/alternatives/jre"}
-echo "JAVA_HOME: ${JAVA_HOME}" >&2
+loggy "JAVA_HOME: $JAVA_HOME"
 
 export SLEEP_INTERVAL=${SLEEP_INTERVAL:-60}
-echo "SLEEP_INTERVAL: ${SLEEP_INTERVAL} seconds." >&2
+loggy "SLEEP_INTERVAL: $SLEEP_INTERVAL seconds."
 
 export SERVER_HELP_EMAIL=${SERVER_HELP_EMAIL:-"not_set"}
-echo "SERVER_HELP_EMAIL: ${SERVER_HELP_EMAIL}" >&2
+loggy "SERVER_HELP_EMAIL: $SERVER_HELP_EMAIL"
 
 export FOLLOW_SYMLINKS=${FOLLOW_SYMLINKS:-"false"}
-echo "FOLLOW_SYMLINKS: ${FOLLOW_SYMLINKS}" >&2
+loggy "FOLLOW_SYMLINKS: $FOLLOW_SYMLINKS"
 
 export NCWMS_BASE=${NCWMS_BASE:-"https://localhost:8080"}
-echo "NCWMS_BASE: ${NCWMS_BASE}" >&2
+loggy "NCWMS_BASE: $NCWMS_BASE"
 
 # AWS ##########################################################################
-echo "${HR2}" >&2
-if test -n "${AWS_ACCESS_KEY_ID}"
+loggy "$HR2"
+if test -n "$AWS_ACCESS_KEY_ID"
 then
-    echo "# AWS_ACCESS_KEY_ID: HAS BEEN SET" >&2
+    loggy "AWS_ACCESS_KEY_ID: HAS BEEN SET"
 else
-    echo "# AWS_ACCESS_KEY_ID: HAS NOT BEEN SET" >&2
+    loggy "AWS_ACCESS_KEY_ID: HAS NOT BEEN SET"
 fi
 
-if test -n "${AWS_SECRET_ACCESS_KEY}"
+if test -n "$AWS_SECRET_ACCESS_KEY"
 then
-    echo "# AWS_SECRET_ACCESS_KEY: HAS BEEN SET" >&2
+    loggy "AWS_SECRET_ACCESS_KEY: HAS BEEN SET"
 else
-    echo "# AWS_SECRET_ACCESS_KEY: HAS NOT BEEN SET" >&2
+    loggy "AWS_SECRET_ACCESS_KEY: HAS NOT BEEN SET"
 fi
 
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-"us-west-2"}
-echo "#    AWS_DEFAULT_REGION: ${AWS_DEFAULT_REGION}" >&2
+loggy "       AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION"
 
 
 export DEBUG=${DEBUG:-false}
+loggy "DEBUG: $DEBUG"
 
 while getopts "de:sn:i:k:r:" opt; do
-  echo "Processing command line opt: ${opt}" >&2
+  loggy "Processing command line opt: $opt"
   case $opt in
     e)
       export SERVER_HELP_EMAIL=$OPTARG
-      echo "Set server admin contact email to: ${SERVER_HELP_EMAIL}" >&2
+      loggy "Set server admin contact email to: $SERVER_HELP_EMAIL"
       ;;
     s)
       export FOLLOW_SYMLINKS="Yes"
-      echo "Set FollowSymLinks to: ${FOLLOW_SYMLINKS}" >&2
+      loggy "Set FollowSymLinks to: $FOLLOW_SYMLINKS"
       ;;
     n)
       export NCWMS_BASE=$OPTARG
-      echo "Set ncWMS public facing service base to : ${NCWMS_BASE}" >&2
+      loggy "Set ncWMS public facing service base to : $NCWMS_BASE"
       ;;
     d)
       DEBUG=true
-      echo "Debug is enabled" >&2
+      loggy "Debug is enabled"
       ;;
     i)
-      export AWS_ACCESS_KEY_ID="${OPTARG}"
-      echo "Found command line value for AWS_ACCESS_KEY_ID." >&2;
+      export AWS_ACCESS_KEY_ID="$OPTARG"
+      loggy "Found command line value for AWS_ACCESS_KEY_ID.";
       ;;
     k)
-      export AWS_SECRET_ACCESS_KEY="${OPTARG}"
-      echo "Found command line value for AWS_SECRET_ACCESS_KEY." >&2;
+      export AWS_SECRET_ACCESS_KEY="$OPTARG"
+      loggy "Found command line value for AWS_SECRET_ACCESS_KEY.";
       ;;
     r)
-      export AWS_DEFAULT_REGION="${OPTARG}"
-      echo "Found command line value for AWS_DEFAULT_REGION: ${AWS_DEFAULT_REGION}" >&2;
+      export AWS_DEFAULT_REGION="$OPTARG"
+      loggy "Found command line value for AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION";
       ;;
 
     \?)
-      echo "Invalid option: -$OPTARG" >&2
-      echo "options: [-e xxx] [-n yyy] [-s] [-d] [-i xxx] [-k xxx] [-r xxx]" >&2
-      echo " -e xxx where xxx is the email address of the admin contact for the server." >&2
-      echo " -s When present causes the BES to follow symbolic links." >&2
-      echo " -n yyy where yyy is the protocol, server and port part "  >&2
-      echo "    of the ncWMS service (for example http://foo.com:8090)."  >&2
-      echo " -d Enables debugging output for this script."  >&2
-      echo " -i xxx Where xxx is an AWS CLI AWS_ACCESS_KEY_ID." >&2
-      echo " -k xxx Where xxx is an AWS CLI AWS_SECRET_ACCESS_KEY." >&2
-      echo " -r xxx Where xxx is an AWS CLI AWS_DEFAULT_REGION." >&2
-      echo "EXITING NOW"  >&2
+      loggy "Invalid option: -$OPTARG"
+      loggy "options: [-e xxx] [-n yyy] [-s] [-d] [-i xxx] [-k xxx] [-r xxx]"
+      loggy " -e xxx where xxx is the email address of the admin contact for the server."
+      loggy " -s When present causes the BES to follow symbolic links."
+      loggy " -n yyy where yyy is the protocol, server and port part "
+      loggy "    of the ncWMS service (for example http://foo.com:8090)."
+      loggy " -d Enables debugging output for this script."
+      loggy " -i xxx Where xxx is an AWS CLI AWS_ACCESS_KEY_ID."
+      loggy " -k xxx Where xxx is an AWS CLI AWS_SECRET_ACCESS_KEY."
+      loggy " -r xxx Where xxx is an AWS CLI AWS_DEFAULT_REGION."
+      loggy "EXITING NOW"
       exit 2
       ;;
   esac
 done
 
-if test "${DEBUG}" = "true" ; then
-    echo "CATALINA_HOME: ${CATALINA_HOME}"  >&2
-    ls -l "$CATALINA_HOME" "$CATALINA_HOME/bin"  >&2
+if test "$DEBUG" = "true" ; then
+    loggy "CATALINA_HOME: $CATALINA_HOME"
+    ls -l "$CATALINA_HOME" "$CATALINA_HOME/bin"
 fi
 
 if test "${DEBUG}" = "true" ; then
-    echo "NCWMS: Using NCWMS_BASE: ${NCWMS_BASE}"  >&2
-    echo "NCWMS: Setting ncWMS access URLs in viewers.xml (if needed)."  >&2
-    ls -l "${VIEWERS_XML}" >&2
+    loggy "NCWMS: Using NCWMS_BASE: $NCWMS_BASE"
+    loggy "NCWMS: Setting ncWMS access URLs in viewers.xml (if needed)."
+    ls -l "$VIEWERS_XML"
 fi
 
-
-sed -i "s+@NCWMS_BASE@+$NCWMS_BASE+g" ${CATALINA_HOME}/webapps/opendap/WEB-INF/conf/viewers.xml
-if test "${DEBUG}" = "true" ; then
-    echo "${CATALINA_HOME}/webapps/opendap/WEB-INF/conf/viewers.xml"  >&2
-    cat ${CATALINA_HOME}/webapps/opendap/WEB-INF/conf/viewers.xml >&2
+sed -i "s+@NCWMS_BASE@+$NCWMS_BASE+g" "$CATALINA_HOME/webapps/opendap/WEB-INF/conf/viewers.xml"
+if test "$DEBUG" = "true" ; then
+    loggy "${CATALINA_HOME}/webapps/opendap/WEB-INF/conf/viewers.xml"
+    loggy "$(cat "${CATALINA_HOME}/webapps/opendap/WEB-INF/conf/viewers.xml")"
 fi
 
 #-------------------------------------------------------------------------------
 # modify bes.conf based on environment variables before startup.
 #
-if test "${SERVER_HELP_EMAIL}" != "not_set" ; then
-    echo "Setting Admin Contact To: $SERVER_HELP_EMAIL"
-    sed -i "s/admin.email.address@your.domain.name/$SERVER_HELP_EMAIL/" /etc/bes/bes.conf
+if test "$SERVER_HELP_EMAIL" != "not_set" ; then
+    loggy "Setting Admin Contact To: $SERVER_HELP_EMAIL"
+    sed -i "s/admin.email.address@your.domain.name/$SERVER_HELP_EMAIL/" "/etc/bes/bes.conf"
 fi
-if test "${FOLLOW_SYMLINKS}" != "not_set" ; then
-    echo "Setting BES FollowSymLinks to YES." >&2
-    sed -i "s/^BES.Catalog.catalog.FollowSymLinks=No/BES.Catalog.catalog.FollowSymLinks=Yes/" /etc/bes/bes.conf
+if test "$FOLLOW_SYMLINKS" != "not_set" ; then
+    loggy "Setting BES FollowSymLinks to YES."
+    sed -i "s/^BES.Catalog.catalog.FollowSymLinks=No/BES.Catalog.catalog.FollowSymLinks=Yes/" "/etc/bes/bes.conf"
 fi
 
-echo "JAVA VERSION: $(java -version)"
+loggy "JAVA VERSION: $(java -version)"
 
-echo "Checking AWS CLI: $(which aws)"
-aws configure list >&2
+loggy "Checking AWS CLI..."
+set +e
+which aws
 status=$?
-if test $status -ne 0 ; then
-    echo "WARNING: Problem with AWS CLI configuration! (status: $status)" >&2
+set -e
+if test $status -ne 0
+then
+    loggy "WARNING: AWS CLI not detected on PATH, may not be installed."
+else
+    aws configure list
+    status=$?
+    if test $status -ne 0 ; then
+        loggy "WARNING: Problem with AWS CLI configuration! (status: $status)"
+    fi
 fi
 
-echo "PythonVersion: $(python3 --version)" >&2
+loggy "PythonVersion: $(python3 --version)"
 #-------------------------------------------------------------------------------
 # Start the BES daemon process
 # /usr/bin/besdaemon -i /usr -c /etc/bes/bes.conf -r /var/run/bes.pid
 bes_uid=$(id -u bes)
-echo "bes_uid: ${bes_uid}" >&2
+loggy "bes_uid: $bes_uid"
 bes_gid=$(id -g bes)
-echo "bes_gid: ${bes_gid}" >&2
+loggy "bes_gid: $bes_gid"
 
-echo "Launching besd..." >&2
+loggy "Launching besd..."
 /usr/bin/besctl start
 status=$?
 if test $status -ne 0 ; then
-    echo "ERROR: Failed to start BES: $status" >&2
+    loggy "ERROR: Failed to start BES: $status"
     exit $status
 fi
-besd_pid=`ps aux | grep /usr/bin/besdaemon | grep -v grep | awk '{print $2;}' - `
-echo "The besd is UP! [pid: ${besd_pid}]" >&2
+besd_pid=$(ps aux | grep "/usr/bin/besdaemon" | grep -v grep | awk '{print $2;}' - )
+loggy "The besd is UP! [pid: $besd_pid]"
 
 #-------------------------------------------------------------------------------
 # Start Tomcat process
 #
 export OLFS_CONF="${CATALINA_HOME}/webapps/opendap/WEB-INF/conf"
 # mv ${OLFS_CONF}/logback.xml ${OLFS_CONF}/logback.xml.OFF
-echo "Starting Tomcat..." >&2
+loggy "Starting Tomcat..."
 #systemctl start tomcat
-${CATALINA_HOME}/bin/startup.sh 2>&1 > /var/log/tomcat/console.log &
+"$CATALINA_HOME"/bin/startup.sh > /var/log/tomcat/console.log  2>&1 &
 status=$?
-tomcat_pid=$!
+tomcat_pid=$! # The $! is the PID of the most recently executed background command.
 if test $status -ne 0 ; then
-    echo "ERROR: Failed to start Tomcat: $status" >&2
+    loggy "ERROR: Failed to start Tomcat: $status"
     exit $status
 fi
 # When we launch tomcat the initial pid gets "retired" because it spawns a
 # secondary processes.
-initial_pid="${tomcat_pid}"
-echo "Tomcat started, initial pid: ${initial_pid}" >&2
+initial_pid=$tomcat_pid
+loggy "Tomcat started, initial pid: $initial_pid"
 while test $initial_pid -eq $tomcat_pid
 do
     sleep 1
     tomcat_ps=$(ps aux | grep tomcat | grep -v grep)
-    echo "tomcat_ps: ${tomcat_ps}" >&2
-    tomcat_pid=$(echo ${tomcat_ps} | awk '{print $2}')
-    echo "tomcat_pid: ${tomcat_pid}" >&2
+    loggy "tomcat_ps: $tomcat_ps"
+    tomcat_pid=$(echo "$tomcat_ps" | awk '{print $2}')
+    loggy "tomcat_pid: $tomcat_pid"
 done
 # New pid and we should be good to go.
-echo "Tomcat is UP! pid: ${tomcat_pid}" >&2
+loggy "Tomcat is UP! pid: $tomcat_pid"
 
 # TEMPORARY
 /cleanup_files.sh >&2 &
 # TEMPORARY
 
-echo "Hyrax Has Arrived..." >&2
-echo "--------------------------------------------------------------------" >&2
+loggy "Hyrax Has Arrived..."
+loggy "--------------------------------------------------------------------"
 #-------------------------------------------------------------------------------
 while /bin/true; do
-    sleep ${SLEEP_INTERVAL}
-    echo "Checking Hyrax Operational State..." >&2
-    besd_ps=`ps -f $besd_pid`
+    sleep "$SLEEP_INTERVAL"
+    loggy "Checking Hyrax Operational State..."
+    besd_ps="$(ps -f "$besd_pid")"
     BESD_STATUS=$?
-    echo "BESD_STATUS: ${BESD_STATUS}" >&2
+    loggy "BESD_STATUS: ${BESD_STATUS}"
 
-    tomcat_ps=$(ps -f "${tomcat_pid}")
+    tomcat_ps="$(ps -f "${tomcat_pid}")"
     TOMCAT_STATUS=$?
-    echo "TOMCAT_STATUS: ${TOMCAT_STATUS}" >&2
+    loggy "TOMCAT_STATUS: $TOMCAT_STATUS"
 
     if test $BESD_STATUS -ne 0 ; then
-        echo "BESD_STATUS: $BESD_STATUS bes_pid:$bes_pid" >&2
-        echo "The BES daemon appears to have died! Exiting." >&2
+        loggy "BESD_STATUS: $BESD_STATUS bes_pid:$bes_pid"
+        loggy "The BES daemon appears to have died! Exiting."
         exit 1
     fi
     if test $TOMCAT_STATUS -ne 0 ; then
-        echo "TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid" >&2
-        echo "Tomcat appears to have died! Exiting." >&2
-        echo "Tomcat Console Log [BEGIN]" >&2
-        cat /var/log/tomcat/console.log >&2
-        echo "Tomcat Console Log [END]" >&2
-        echo "catalina.out [BEGIN]" >&2
-        cat /usr/share/tomcat/logs/catalina.out >&2
-        echo "catalina.out [END]" >&2
-        echo "localhost.log [BEGIN]" >&2
-        cat /usr/share/tomcat/logs/localhost* >&2
-        echo "localhost.log [END]" >&2
+        loggy "TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid"
+        loggy "Tomcat appears to have died! Exiting."
+        loggy "Tomcat Console Log [BEGIN]"
+        loggy "$(cat /var/log/tomcat/console.log)"
+        loggy "Tomcat Console Log [END]"
+        loggy "catalina.out [BEGIN]"
+        loggy "$(cat /usr/share/tomcat/logs/catalina.out)"
+        loggy "catalina.out [END]"
+        loggy "localhost.log [BEGIN]"
+        loggy "$(cat /usr/share/tomcat/logs/localhost*)"
+        loggy "localhost.log [END]"
         exit 2
     fi
     
     if test "${DEBUG}" = "true" ; then
-        echo "-------------------------------------------------------------------"  >&2
-        date >&2
-        echo "BESD_STATUS: $BESD_STATUS  besd_pid:$besd_pid" >&2
-        echo "TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid" >&2
+        loggy "-------------------------------------------------------------------"
+        date
+        loggy "BESD_STATUS: $BESD_STATUS  besd_pid:$besd_pid"
+        loggy "TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid"
     fi
 done
 #-------------------------------------------------------------------------------
