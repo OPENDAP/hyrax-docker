3b959717cf70031deda2fdf7966b03d98e53e1cd
ndp-opendap <ndp@opendap.org>
2026-02-17 12:06:46 -0800

    Working hyrax el9 docker

--- el9-builds/hyrax/entrypoint.sh
+++ el9-builds/hyrax/entrypoint.sh
@@ -21,114 +21,114 @@ loggy "Greetings, I am "`whoami`" (uid: "`echo ${UID}`")."
 # set -e
 # set -x
 
-loggy "$prolog        PythonVersion: "$(python3 --version)
+loggy "$plog        PythonVersion: "$(python3 --version)
 
 export JAVA_HOME=${JAVA_HOME:-"/etc/alternatives/jre"}
-loggy "$prolog            JAVA_HOME: $JAVA_HOME"
+loggy "$plog            JAVA_HOME: $JAVA_HOME"
 
 export SLEEP_INTERVAL=${SLEEP_INTERVAL:-60}
-loggy "$prolog       SLEEP_INTERVAL: $SLEEP_INTERVAL seconds."
+loggy "$plog       SLEEP_INTERVAL: $SLEEP_INTERVAL seconds."
 
 export SERVER_HELP_EMAIL=${SERVER_HELP_EMAIL:-"not_set"}
-loggy "$prolog    SERVER_HELP_EMAIL: $SERVER_HELP_EMAIL"
+loggy "$plog    SERVER_HELP_EMAIL: $SERVER_HELP_EMAIL"
 
 export FOLLOW_SYMLINKS=${FOLLOW_SYMLINKS:-"false"}
-loggy "$prolog      FOLLOW_SYMLINKS: $FOLLOW_SYMLINKS"
+loggy "$plog      FOLLOW_SYMLINKS: $FOLLOW_SYMLINKS"
 
 export NCWMS_BASE=${NCWMS_BASE:-"https://localhost:8080"}
-loggy "$prolog           NCWMS_BASE: $NCWMS_BASE"
+loggy "$plog           NCWMS_BASE: $NCWMS_BASE"
 
 # AWS ##########################################################################
 loggy "$HR2"
 if test -n "$AWS_ACCESS_KEY_ID"
 then
-    loggy "$prolog     AWS_ACCESS_KEY_ID: HAS BEEN SET"
+    loggy "$plog     AWS_ACCESS_KEY_ID: HAS BEEN SET"
 else
-    loggy "$prolog     AWS_ACCESS_KEY_ID: HAS NOT BEEN SET"
+    loggy "$plog     AWS_ACCESS_KEY_ID: HAS NOT BEEN SET"
 fi
 
 if test -n "$AWS_SECRET_ACCESS_KEY"
 then
-    loggy "$prolog AWS_SECRET_ACCESS_KEY: HAS BEEN SET"
+    loggy "$plog AWS_SECRET_ACCESS_KEY: HAS BEEN SET"
 else
-    loggy "$prolog AWS_SECRET_ACCESS_KEY: HAS NOT BEEN SET"
+    loggy "$plog AWS_SECRET_ACCESS_KEY: HAS NOT BEEN SET"
 fi
 
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-"us-west-2"}
-loggy " $prolog   AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION"
+loggy " $plog   AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION"
 
 
 export DEBUG=${DEBUG:-"true"}
-loggy "$prolog                 DEBUG: $DEBUG"
+loggy "$plog                 DEBUG: $DEBUG"
 
-loggy "$prolog Processing Command Line Options."
+loggy "$plog Processing Command Line Options."
 while getopts "de:sn:i:k:r:" opt; do
   loggy "Processing command line opt: $opt"
   case $opt in
     e)
       export SERVER_HELP_EMAIL=$OPTARG
-      loggy "$prolog Set server admin contact email to: $SERVER_HELP_EMAIL"
+      loggy "$plog Set server admin contact email to: $SERVER_HELP_EMAIL"
       ;;
     s)
       export FOLLOW_SYMLINKS="Yes"
-      loggy "$prolog Set FollowSymLinks to: $FOLLOW_SYMLINKS"
+      loggy "$plog Set FollowSymLinks to: $FOLLOW_SYMLINKS"
       ;;
     n)
       export NCWMS_BASE=$OPTARG
-      loggy "$prolog Set ncWMS public facing service base to : $NCWMS_BASE"
+      loggy "$plog Set ncWMS public facing service base to : $NCWMS_BASE"
       ;;
     d)
       DEBUG=true
-      loggy "$prolog Debug is enabled"
+      loggy "$plog Debug is enabled"
       ;;
     i)
       export AWS_ACCESS_KEY_ID="$OPTARG"
-      loggy "$prolog Found command line value for AWS_ACCESS_KEY_ID.";
+      loggy "$plog Found command line value for AWS_ACCESS_KEY_ID.";
       ;;
     k)
       export AWS_SECRET_ACCESS_KEY="$OPTARG"
-      loggy "$prolog Found command line value for AWS_SECRET_ACCESS_KEY.";
+      loggy "$plog Found command line value for AWS_SECRET_ACCESS_KEY.";
       ;;
     r)
       export AWS_DEFAULT_REGION="$OPTARG"
-      loggy "$prolog Found command line value for AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION";
+      loggy "$plog Found command line value for AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION";
       ;;
 
     \?)
-      loggy "$prolog Invalid option: -$OPTARG"
-      loggy "$prolog options: [-e xxx] [-n yyy] [-s] [-d] [-i xxx] [-k xxx] [-r xxx]"
-      loggy "$prolog  -e xxx where xxx is the email address of the admin contact for the server."
-      loggy "$prolog  -s When present causes the BES to follow symbolic links."
-      loggy "$prolog  -n yyy where yyy is the protocol, server and port part "
-      loggy "$prolog     of the ncWMS service (for example http://foo.com:8090)."
-      loggy "$prolog  -d Enables debugging output for this script."
-      loggy "$prolog  -i xxx Where xxx is an AWS CLI AWS_ACCESS_KEY_ID."
-      loggy "$prolog  -k xxx Where xxx is an AWS CLI AWS_SECRET_ACCESS_KEY."
-      loggy "$prolog  -r xxx Where xxx is an AWS CLI AWS_DEFAULT_REGION."
-      loggy "$prolog EXITING NOW"
+      loggy "$plog Invalid option: -$OPTARG"
+      loggy "$plog options: [-e xxx] [-n yyy] [-s] [-d] [-i xxx] [-k xxx] [-r xxx]"
+      loggy "$plog  -e xxx where xxx is the email address of the admin contact for the server."
+      loggy "$plog  -s When present causes the BES to follow symbolic links."
+      loggy "$plog  -n yyy where yyy is the protocol, server and port part "
+      loggy "$plog     of the ncWMS service (for example http://foo.com:8090)."
+      loggy "$plog  -d Enables debugging output for this script."
+      loggy "$plog  -i xxx Where xxx is an AWS CLI AWS_ACCESS_KEY_ID."
+      loggy "$plog  -k xxx Where xxx is an AWS CLI AWS_SECRET_ACCESS_KEY."
+      loggy "$plog  -r xxx Where xxx is an AWS CLI AWS_DEFAULT_REGION."
+      loggy "$plog EXITING NOW"
       exit 2
       ;;
   esac
 done
-loggy "$prolog Command Line Options Have Been Processed."
+loggy "$plog Command Line Options Have Been Processed."
 loggy "$HR0"
 
 if test "$DEBUG" = "true" ; then
-    loggy "$prolog CATALINA_HOME: $CATALINA_HOME"
+    loggy "$plog CATALINA_HOME: $CATALINA_HOME"
     loggy "$(ls -l "$CATALINA_HOME")"
     loggy "$(ls -l "$CATALINA_HOME/bin")"
 fi
 
 export VIEWERS_XML="$CATALINA_HOME/webapps/opendap/WEB-INF/conf/viewers.xml"
 if test "$DEBUG" = "true" ; then
-    loggy "$prolog NCWMS: Using NCWMS_BASE: $NCWMS_BASE"
-    loggy "$prolog NCWMS: Setting ncWMS access URLs in viewers.xml (if needed)."
+    loggy "$plog NCWMS: Using NCWMS_BASE: $NCWMS_BASE"
+    loggy "$plog NCWMS: Setting ncWMS access URLs in viewers.xml (if needed)."
     loggy "$(ls -l "$VIEWERS_XML")"
 fi
 
 sed -i "s+@NCWMS_BASE@+$NCWMS_BASE+g" "$VIEWERS_XML"
 if test "$DEBUG" = "true" ; then
-    loggy "$prolog $VIEWERS_XML"
+    loggy "$plog $VIEWERS_XML"
     loggy "$(cat "$VIEWERS_XML")"
 fi
 
@@ -136,80 +136,80 @@ fi
 # modify bes.conf based on environment variables before startup.
 #
 if test "$SERVER_HELP_EMAIL" != "not_set" ; then
-    loggy "$prolog Setting Admin Contact To: $SERVER_HELP_EMAIL"
+    loggy "$plog Setting Admin Contact To: $SERVER_HELP_EMAIL"
     sed -i "s/admin.email.address@your.domain.name/$SERVER_HELP_EMAIL/" "/etc/bes/bes.conf"
 fi
 if test "$FOLLOW_SYMLINKS" != "not_set" ; then
-    loggy "$prolog Setting BES FollowSymLinks to YES."
+    loggy "$plog Setting BES FollowSymLinks to YES."
     sed -i "s/^BES.Catalog.catalog.FollowSymLinks=No/BES.Catalog.catalog.FollowSymLinks=Yes/" "/etc/bes/bes.conf"
 fi
 
-loggy "$prolog JAVA VERSION: $( java -version 2>&1 )"
+loggy "$plog JAVA VERSION: $( java -version 2>&1 )"
 
-loggy "$prolog Checking for awscli"
+loggy "$plog Checking for awscli"
 set +e
 which_output=$(eval "which aws" 2>&1)
 status=$?
 set -e
-loggy "$prolog $which_output"
+loggy "$plog $which_output"
 if test $status -ne 0
 then
-    loggy "$prolog WARNING: AWS CLI not detected on PATH, may not be installed."
+    loggy "$plog WARNING: AWS CLI not detected on PATH, may not be installed."
 else
     aws configure list
     status=$?
     if test $status -ne 0 ; then
-        loggy "$prolog WARNING: Problem with AWS CLI configuration! (status: $status)"
+        loggy "$plog WARNING: Problem with AWS CLI configuration! (status: $status)"
     fi
 fi
 
-loggy "$prolog PythonVersion: $( python3 --version 2>&1 )"
+loggy "$plog PythonVersion: $( python3 --version 2>&1 )"
 #-------------------------------------------------------------------------------
 # Start the BES daemon process
 # /usr/bin/besdaemon -i /usr -c /etc/bes/bes.conf -r /var/run/bes.pid
 bes_uid=$(id -u bes)
-loggy "$prolog bes_uid: $bes_uid"
+loggy "$plog bes_uid: $bes_uid"
 bes_gid=$(id -g bes)
-loggy "$prolog bes_gid: $bes_gid"
+loggy "$plog bes_gid: $bes_gid"
 
-loggy "$prolog Launching besd..."
+loggy "$plog Launching besd..."
 /usr/bin/besctl start
 status=$?
 if test $status -ne 0 ; then
-    loggy "$prolog ERROR: Failed to start BES: $status"
+    loggy "$plog ERROR: Failed to start BES: $status"
     exit $status
 fi
 besd_pid=$(ps aux | grep "/usr/bin/besdaemon" | grep -v grep | awk '{print $2;}' - )
-loggy "$prolog The besd is UP! [pid: $besd_pid]"
+loggy "$plog The besd is UP! [pid: $besd_pid]"
 
 #-------------------------------------------------------------------------------
 # Start Tomcat process
 #
 export OLFS_CONF="${CATALINA_HOME}/webapps/opendap/WEB-INF/conf"
 # mv ${OLFS_CONF}/logback.xml ${OLFS_CONF}/logback.xml.OFF
-loggy "$prolog Starting Tomcat..."
+loggy "$plog Starting Tomcat..."
 #systemctl start tomcat
 "$CATALINA_HOME"/bin/startup.sh > /var/log/tomcat/console.log  2>&1 &
 status=$?
 tomcat_pid=$! # The $! is the PID of the most recently executed background command.
 if test $status -ne 0 ; then
-    loggy "$prolog ERROR: Failed to start Tomcat: $status"
+    loggy "$plog ERROR: Failed to start Tomcat: $status"
     exit $status
 fi
 # When we launch tomcat the initial pid gets "retired" because it spawns a
 # secondary processes.
 initial_pid=$tomcat_pid
-loggy "$prolog Tomcat started, initial pid: $initial_pid"
+loggy "$plog Tomcat started, initial pid: $initial_pid"
 while test $initial_pid -eq $tomcat_pid
 do
     sleep 1
     tomcat_ps=$(ps aux | grep tomcat | grep -v grep)
-    loggy "$prolog tomcat_ps: $tomcat_ps"
+    loggy "$plog tomcat_ps: $tomcat_ps"
     tomcat_pid=$(echo "$tomcat_ps" | awk '{print $2}')
-    loggy "$prolog tomcat_pid: $tomcat_pid"
+    loggy "$plog tomcat_pid: $tomcat_pid"
 done
 # New pid and we should be good to go.
-loggy "$prolog Tomcat is UP! pid: $tomcat_pid"
+loggy "$plog Tomcat is UP! pid: $tomcat_pid"
 
 # TEMPORARY
 /cleanup_files.sh >&2 &
@@ -219,45 +219,45 @@ loggy "$prolog Tomcat is UP! pid: $tomcat_pid"
 
 #-------------------------------------------------------------------------------
 loggy "$HR1"
-loggy "$prolog Hyrax Has Arrived..."
+loggy "$plog Hyrax Has Arrived..."
 loggy "$HR2"
 while /bin/true; do
-    loggy "$prolog Hyrax Check BEGIN. SLEEP_INTERVAL: '$SLEEP_INTERVAL'"
+    loggy "$plog Hyrax Check BEGIN. SLEEP_INTERVAL: '$SLEEP_INTERVAL'"
     sleep 60
-    loggy "$prolog Checking Hyrax Operational State..."
+    loggy "$plog Checking Hyrax Operational State..."
     besd_ps="$(ps -f "$besd_pid")"
     BESD_STATUS=$?
-    loggy "$prolog BESD_STATUS: $BESD_STATUS"
+    loggy "$plog BESD_STATUS: $BESD_STATUS"
 
     tomcat_ps="$(ps -f "${tomcat_pid}")"
     TOMCAT_STATUS=$?
-    loggy "$prolog TOMCAT_STATUS: $TOMCAT_STATUS"
+    loggy "$plog TOMCAT_STATUS: $TOMCAT_STATUS"
 
     if test $BESD_STATUS -ne 0 ; then
-        loggy "$prolog BESD_STATUS: $BESD_STATUS besd_ps:$besd_ps"
-        loggy "$prolog The BES daemon appears to have died! Exiting."
+        loggy "$plog BESD_STATUS: $BESD_STATUS besd_ps:$besd_ps"
+        loggy "$plog The BES daemon appears to have died! Exiting."
         exit 1
     fi
     if test $TOMCAT_STATUS -ne 0 ; then
-        loggy "$prolog TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid"
-        loggy "$prolog Tomcat appears to have died! Exiting."
-        loggy "$prolog Tomcat Console Log [BEGIN]"
+        loggy "$plog TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid"
+        loggy "$plog Tomcat appears to have died! Exiting."
+        loggy "$plog Tomcat Console Log [BEGIN]"
         loggy "$(cat /var/log/tomcat/console.log)"
-        loggy "$prolog Tomcat Console Log [END]"
-        loggy "$prolog catalina.out [BEGIN]"
+        loggy "$plog Tomcat Console Log [END]"
+        loggy "$plog catalina.out [BEGIN]"
         loggy "$(cat /usr/share/tomcat/logs/catalina.out)"
-        loggy "$prolog catalina.out [END]"
-        loggy "$prolog localhost.log [BEGIN]"
+        loggy "$plog catalina.out [END]"
+        loggy "$plog localhost.log [BEGIN]"
         loggy "$(cat /usr/share/tomcat/logs/localhost*)"
-        loggy "$prolog localhost.log [END]"
+        loggy "$plog localhost.log [END]"
         exit 2
     fi
     
     if test "${DEBUG}" = "true" ; then
         loggy "-------------------------------------------------------------------"
         date
-        loggy "$prolog BESD_STATUS: $BESD_STATUS  besd_pid:$besd_pid"
-        loggy "$prolog TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid"
+        loggy "$plog BESD_STATUS: $BESD_STATUS  besd_pid:$besd_pid"
+        loggy "$plog TOMCAT_STATUS: $TOMCAT_STATUS tomcat_pid:$tomcat_pid"
     fi
 done
 #-------------------------------------------------------------------------------
