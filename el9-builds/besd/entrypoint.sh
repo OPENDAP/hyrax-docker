ab3f4a083fc3b5bb1fcbe87ae61e5aff3d5b97ea
Hannah Robertson <1076701+hannahilea@users.noreply.github.com>
2026-03-04 17:08:22 +0000

    Update el-9 shared and besd build

--- el9-builds/besd/entrypoint.sh
+++ el9-builds/besd/entrypoint.sh
@@ -112,11 +112,11 @@ fi
 #
 if [ "$SERVER_HELP_EMAIL" != "not_set" ]; then
     loggy "Setting Admin Contact To: $SERVER_HELP_EMAIL"
-    sed -i "s/admin.email.address@your.domain.name/$SERVER_HELP_EMAIL/" /etc/bes/bes.conf
+    sed -i "s/admin.email.address@your.domain.name/$SERVER_HELP_EMAIL/" $PREFIX/etc/bes/bes.conf
 fi
 if [ "$FOLLOW_SYMLINKS" != "not_set" ]; then
     loggy "Setting BES FollowSymLinks to YES."
-    sed -i "s/^BES.Catalog.catalog.FollowSymLinks=No/BES.Catalog.catalog.FollowSymLinks=Yes/" /etc/bes/bes.conf
+    sed -i "s/^BES.Catalog.catalog.FollowSymLinks=No/BES.Catalog.catalog.FollowSymLinks=Yes/" $PREFIX/etc/bes/bes.conf
 fi
 
 #-------------------------------------------------------------------------------
@@ -128,16 +128,16 @@ bes_gid="$(id -g bes)"
 echo "bes_gid: $bes_gid"
 
 # Start the BES daemon process
-# /usr/bin/besdaemon -i /usr -c /etc/bes/bes.conf -r /var/run/bes.pid
+# $PREFIX/usr/bin/besdaemon -i $PREFIX/usr -c $PREFIX/etc/bes/bes.conf -r /var/run/bes.pid
 loggy "Calling 'besctl start'"
-/usr/bin/besctl start
+$PREFIX/usr/bin/besctl start
 status=$?
 if [ $status -ne 0 ]; then
     loggy "ERROR: Failed to start BES: $status"
     exit $status
 fi
 
-besd_pid="$(ps aux | grep /usr/bin/besdaemon | grep -v grep | awk '{print $2;}' - )"
+besd_pid="$(ps aux | grep $PREFIX/usr/bin/besdaemon | grep -v grep | awk '{print $2;}' - )"
 loggy "The besdaemon is UP! pid: $besd_pid"
 start_time=
 start_time="$(date  "+%s")"
