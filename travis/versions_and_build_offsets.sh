5a6b24f434a4044fdca643161061c7a25df2649b
ndp-opendap <ndp@opendap.org>
2026-03-10 12:56:09 -0700

    First pass at changing Hyrax build numbers to be set by hyrax-docker Travis jobs

---
+++ travis/versions_and_build_offsets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# When the release numbers are edited for a release, update these
+# to the current Travis number so that the 'build number' in
+# x.y.z-<build number> is zero. jhrg 3/22/21
+
+export HYRAX_RELEASE_VERSION=1.17.1
+export TRAVIS_HYRAX_BUILD_OFFSET=2410
+echo "#     HYRAX_RELEASE_VERSION: $HYRAX_RELEASE_VERSION" >&2
+echo "# TRAVIS_HYRAX_BUILD_OFFSET: $TRAVIS_HYRAX_BUILD_OFFSET" >&2
