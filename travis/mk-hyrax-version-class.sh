56bfe43b0702ad7952a404ddadb91f0319e5f72b
ndp-opendap <ndp@opendap.org>
2026-03-11 11:52:18 -0700

    Collecting and logging version response in regression_test_script

--- travis/mk-hyrax-version-class.sh
+++ travis/mk-hyrax-version-class.sh
@@ -13,6 +13,7 @@ EOF
 
 target_dir="$TRAVIS_BUILD_DIR/$TARGET_OS-builds/$DOCKER_DIR"
 echo "# target_dir: $target_dir" >&2
+
 target_file="$target_dir/HyraxVersion.java"
 echo "# target_file: $target_file" >&2
 
