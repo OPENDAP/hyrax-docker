9462d2ce2be22b20993b2d2c4e1e7f4a52c53d9e
ndp-opendap <ndp@opendap.org>
2026-03-10 17:58:29 -0700

    Working on the HyraxVersion.java injection

--- travis/inject_hyrax_version.sh
+++ travis/inject_hyrax_version.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+hyrax_version_class=$(
+        cat << EOF
+package opendap.version;
 
+public class HyraxVersion {
+    private static final String hyraxVersion = "@HyraxVersion@";
 
+    /**
+     * Returns a String containing the Hyrax version.
+     * @return The version of Hyrax.
+     */
+    public static String getVersionString() { return hyraxVersion; }
 
+}
+EOF
+    )
+
+echo "${hyrax_version_class//@HyraxVersion@/$HYRAX_VERSION}" > HyraxVersion.java
+
+loggy "cat HyraxVersion.java"
+javac HyraxVersion.java
+loggy "$(ls -l HyraxVersion*)"
