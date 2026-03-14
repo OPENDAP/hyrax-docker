b541fa9ac746d784798a8a5efd10f25c9caa0e53
ndp-opendap <ndp@opendap.org>
2026-03-13 20:47:48 -0700

    format fixich

--- travis/tag_this_build.sh
+++ travis/tag_this_build.sh
@@ -12,7 +12,8 @@ tag_this_build() {
     local tag_name
 
     loggy "$prolog BEGIN"
-    if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+        loggy "$prolog Tagging Build. TRAVIS_BRANCH $TRAVIS_BRANCH, TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST " >&2
         loggy "$prolog OS_BUILD_VERSION_TAG is '$OS_BUILD_VERSION_TAG'" >&2
         tag_name="${OS_BUILD_VERSION_TAG//:/@}"
         loggy "$prolog             tag_name is '$tag_name'" >&2
