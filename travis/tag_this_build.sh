56968596d371269b3e1fadac16e38416cb6cf127
Nathan Potter <ndp@opendap.org>
2026-03-16 10:14:38 -0700

    Set up build tagging .travis.yml (#137)
    * Set up build tagging .travis.yml
    * Turning on tagging

---
+++ travis/tag_this_build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+###########################################################################
+# loggy()
+function loggy() {
+    echo "$@" | awk '{ print "# "$0;}' >&2
+}
+
+###########################################################################
+# tag_this_build()
+tag_this_build() {
+    local prolog="tag_this_build() -"
+    local tag_name
+
+    loggy "$prolog BEGIN"
+    if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]
+    then
+        loggy "$prolog Tagging Build. TRAVIS_BRANCH: $TRAVIS_BRANCH, TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST "
+        loggy "$prolog OS_BUILD_VERSION_TAG: '$OS_BUILD_VERSION_TAG'"
+        tag_name="${OS_BUILD_VERSION_TAG//:/@}"
+        loggy "$prolog             tag_name: '$tag_name'"
+        git tag -a "$tag_name" -m "$(cat $TRAVIS_BUILD_DIR/travis-build-recipe)"; \
+        git push origin "$tag_name"
+    else
+        loggy "$prolog Skipping Build Tag. TRAVIS_BRANCH: $TRAVIS_BRANCH, TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST "
+    fi
+    loggy "$prolog END"
+}
+
+tag_this_build
