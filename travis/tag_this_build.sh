76e0ce0c0cc4bc53b5919596427139a8fef8ac09
ndp-opendap <ndp@opendap.org>
2026-03-16 14:20:47 -0700

    Enable early tag, add error status

--- travis/tag_this_build.sh
+++ travis/tag_this_build.sh
@@ -10,6 +10,8 @@ function loggy() {
 tag_this_build() {
     local prolog="tag_this_build() -"
     local tag_name
+    local status
+    local repo_name="hyrax-docker"
 
     loggy "$prolog BEGIN"
     if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]
@@ -18,8 +20,18 @@ tag_this_build() {
         loggy "$prolog OS_BUILD_VERSION_TAG: '$OS_BUILD_VERSION_TAG' GITUID: $GITUID"
         tag_name="${OS_BUILD_VERSION_TAG//:/@}"
         loggy "$prolog             tag_name: '$tag_name'"
-        git tag -a "$tag_name" -m "$(cat $TRAVIS_BUILD_DIR/travis-build-recipe)"
-        git push "https://${GIT_UID}:${GIT_PSWD}@github.com/OPENDAP/hyrax-docker.git" "$tag_name"
+        # git tag -a "$tag_name" -m "$(cat $TRAVIS_BUILD_DIR/travis-build-recipe)"
+
+        tag_name="DEBUG-FTW-$TRAVIS_BUILD_NUMBER"
+        git tag -a "$tag_name" -m "Testing tag and push."
+
+        git push "https://${GIT_PSWD}@github.com/OPENDAP/$repo_name.git" "$tag_name"
+        status=$?
+        if $status -ne 0
+        then
+           loggy "$prolog The 'git push attempt' failed."
+           return $status
+        fi
     else
         loggy "$prolog Skipping Build Tag. TRAVIS_BRANCH: $TRAVIS_BRANCH, TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST "
     fi
