0e8515fcf2253dd02f3ce90adece142a997c8c96
ndp-opendap <ndp@opendap.org>
2026-03-28 05:06:12 -0700

    wip

--- travis/test_image_starts_without_crashing.sh
+++ travis/test_image_starts_without_crashing.sh
@@ -32,13 +32,7 @@ function check_version() {
     fi
     loggy "$prolog     deployment_context: $deployment_context"
 
-    local expected_hyrax_version="$3"
-    if test -z "$expected_hyrax_version"; then
-        loggy "$prolog ERROR! You must pass a Hyrax endpoint url string as parameter 4"
-        return 3
-    fi
-    loggy "$prolog expected_hyrax_version: $expected_hyrax_version"
-
+    local expected_version_str="$HYRAX_WEB_UI_VERSION"
     local docker_version_label
     local docker_labels
     local docker_version_status
@@ -46,7 +40,7 @@ function check_version() {
 
 
     loggy "$prolog Checking docker image metadata for correct Hyrax version..."
-    docker_labels=$(docker inspect --format="{{ index .Config.Labels}}" "$d_id")
+    docker_labels="$(docker inspect --format="{{ index .Config.Labels}}" "$d_id")"
     loggy "$prolog docker_labels:"
     loggy "$docker_labels"
     loggy ""
@@ -55,19 +49,21 @@ function check_version() {
     if test "$DOCKER_NAME" = "besd"
     then
         version_label_key="org.opendap.besdaemon.version"
+        expected_version_str="$BES_VERSION"
     fi
-    loggy "$prolog version_label_key: $version_label_key"
+    loggy "$prolog    version_label_key: $version_label_key"
+    loggy "$prolog expected_version_str: $expected_version_str"
 
-    docker_version_label=$(docker inspect --format="{{ index .Config.Labels \"$version_label_key\" }}" "$d_id")
+    docker_version_label="$(docker inspect --format="{{ index .Config.Labels \"$version_label_key\" }}" "$d_id")"
     docker_version_status=$?
     loggy "docker_version_label: '$docker_version_label'"
-    if test "$expected_hyrax_version" = "$docker_version_label"; then
+    if test "$expected_version_str" = "$docker_version_label"; then
         loggy "$prolog SUCCESS!
-        The 'docker inspect' command for $d_id returned the expected_hyrax_version value."
+        The 'docker inspect' command for $d_id returned the expected_version_str value."
     else
         loggy "$prolog ERROR!
         The value of the hyrax version string '$docker_version_label' found in the docker
-        inspect response does not match the expected_hyrax_version env value '$expected_hyrax_version'
+        inspect response does not match the expected_version_str env value '$expected_version_str'
         in this production environment!"
         return 1
     fi
@@ -80,7 +76,7 @@ function check_version() {
         some_page="$(docker exec -it $d_id bash -c "cat /usr/share/tomcat/webapps/$deployment_context/docs/ngap/ngap.html")"
         # loggy "$prolog NGAP Landing Page: "
         # loggy "$some_page"
-        echo "$some_page" | grep "$expected_hyrax_version"
+        echo "$some_page" | grep "$expected_version_str"
         status=$?
         if test $status -ne 0
         then
@@ -89,22 +85,24 @@ function check_version() {
         fi
     fi
 
-    #####################################################################
-    # Check version.xsl
-    #
-    loggy "$HR1"
-    loggy "$prolog Checking version.xsl for correct Hyrax version, d_id: $d_id"
-    some_page="$(docker exec -it "$d_id" bash -c "cat /usr/share/tomcat/webapps/$deployment_context/xsl/version.xsl")"
-    #loggy "$prolog version.xsl: "
-    #loggy "$some_page"
-    echo "$some_page" | grep "$expected_hyrax_version"
-    status=$?
-    if test $status -ne 0
+    if test $DOCKER_NAME != "besd"
     then
-        loggy "$prolog ERROR! The expected version string as not found in the version.xsl file."
-        return  $status
+        #####################################################################
+        # Check version.xsl
+        #
+        loggy "$HR1"
+        loggy "$prolog Checking version.xsl for correct Hyrax version, d_id: $d_id"
+        some_page="$(docker exec -it "$d_id" bash -c "cat /usr/share/tomcat/webapps/$deployment_context/xsl/version.xsl")"
+        #loggy "$prolog version.xsl: "
+        #loggy "$some_page"
+        echo "$some_page" | grep "$expected_version_str"
+        status=$?
+        if test $status -ne 0
+        then
+            loggy "$prolog ERROR! The expected version string as not found in the version.xsl file."
+            return  $status
+        fi
     fi
-
     loggy "$prolog END"
     loggy "$HR0"
     return 0
@@ -120,8 +118,9 @@ function test_startup() {
     local image_tag="$1"
     local any_images_crashed
     local status
+
     loggy "$prolog Test that image does not crash on startup"
-    docker run -d --name=travis_test_image "${image_tag}"
+    docker run -d --name=travis_test_image "$image_tag"
     
     # Wait to give the entrypoint script/application a chance to run
     sleep 10
@@ -139,7 +138,8 @@ function test_startup() {
         sleep 10
         exit 1
     else
-        loggy "$prolog Success: Image '${image_tag}' did not crash on startup"
+        loggy "$prolog Success: Image '$image_tag' did not crash on startup"
+
         check_version "travis_test_image" "ROOT" "$HYRAX_WEB_UI_VERSION"
         status=$?
         if test $status -ne 0
