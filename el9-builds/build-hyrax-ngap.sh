1e8b140b12bf48d800069e03a2595efd86365efb
ndp-opendap <ndp@opendap.org>
2026-06-02 13:05:22 -0700

    Restrict jackson-core version to 2.21.+

--- el9-builds/build-hyrax-ngap.sh
+++ el9-builds/build-hyrax-ngap.sh
@@ -63,11 +63,13 @@ loggy "$HR1"
 loggy "Retrieving Java dependency libraries. (Redisson and ElasticCache Cluster Client Jars)"
 lib_dir="$DOCKER_DIR/lib"
 loggy "lib_dir: $lib_dir"
-loggy "$(set -e; gradle dependencyLibrariesDownload 2>&1 )"
+set -e
+gradle_log=$"(gradle dependencyLibrariesDownload 2>&1 )"
+loggy "$gradle_log"
 loggy "ls -l $lib_dir"
 loggy "$(ls -l "$lib_dir")"
 loggy "$HR1"
-
+set +e
 
 # Make the HyraxVersion assets to be injected into the docker image.
 make_hyrax_version_assets "$HYRAX_WEB_UI_VERSION"
