2e4ed724ce17d384899a031fb5a7133246400c20
ndp-opendap <ndp@opendap.org>
2026-07-24 12:36:54 -0700

    Updating for hyrax-1.18.0

--- travis/versions_and_build_offsets.sh
+++ travis/versions_and_build_offsets.sh
@@ -4,7 +4,7 @@
 # to the current Travis number so that the 'build number' in
 # x.y.z-<build number> is zero.
 
-export HYRAX_RELEASE_VERSION=1.17.1
-export TRAVIS_HYRAX_BUILD_OFFSET=2410
+export HYRAX_RELEASE_VERSION=1.18.0
+export TRAVIS_HYRAX_BUILD_OFFSET=3884
 echo "#     HYRAX_RELEASE_VERSION: $HYRAX_RELEASE_VERSION" >&2
 echo "# TRAVIS_HYRAX_BUILD_OFFSET: $TRAVIS_HYRAX_BUILD_OFFSET" >&2
